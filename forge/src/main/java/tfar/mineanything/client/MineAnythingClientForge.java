de01058a86704abf50d3ff966f044afa30d0fb91
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-05-30 17:00:34 -0400

    Update MineAnythingClientForge.java

--- forge/src/main/java/tfar/mineanything/client/MineAnythingClientForge.java
+++ forge/src/main/java/tfar/mineanything/client/MineAnythingClientForge.java
@@ -1,14 +1,19 @@
 package tfar.mineanything.client;
 
 import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.multiplayer.PlayerInfo;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.player.RemotePlayer;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import net.minecraft.client.resources.DefaultPlayerSkin;
+import net.minecraft.client.resources.SkinManager;
+import net.minecraft.core.UUIDUtil;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
@@ -20,6 +25,7 @@ import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.codehaus.plexus.util.CachedMap;
 import tfar.mineanything.MineAnything;
@@ -36,6 +42,7 @@ public class MineAnythingClientForge {
         bus.addListener(MineAnythingClientForge::registerRenderers);
         MinecraftForge.EVENT_BUS.addListener(MineAnythingClientForge::clientTick);
         MinecraftForge.EVENT_BUS.addListener(MineAnythingClientForge::renderPlayer);
+        MinecraftForge.EVENT_BUS.addListener(MineAnythingClientForge::clientPlayerTick);
     }
 
     static void clientSetup(FMLClientSetupEvent event) {
@@ -45,7 +52,38 @@ public class MineAnythingClientForge {
         MineAnythingClient.clientTick();
     }
 
+    static void clientPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.side == LogicalSide.CLIENT && event.phase == TickEvent.Phase.START) {
+            Player player = event.player;
+            ItemStack headItem = player.getItemBySlot(EquipmentSlot.HEAD);
+            if (headItem.is(Items.PLAYER_HEAD)) {
+                CompoundTag tag = headItem.getTag();
+                if (tag != null && tag.contains(PlayerHeadItem.TAG_SKULL_OWNER)) {
+                    GameProfile gameProfile = NbtUtils.readGameProfile(tag.getCompound(PlayerHeadItem.TAG_SKULL_OWNER));
+                    if (!Objects.equals(player.getGameProfile(), gameProfile)) {
+                        RemotePlayer fakePlayer = playerCache.computeIfAbsent(gameProfile.getId(),uuid -> new RemotePlayer((ClientLevel)player.level(), gameProfile));
+                        fakePlayer.tick();
+                    }
+                }
+            }
+        }
+    }
+
     private static final Map<UUID, RemotePlayer> playerCache = new HashMap<>();
+
+    public static boolean isDisguised(Player player) {
+        ItemStack headItem = player.getItemBySlot(EquipmentSlot.HEAD);
+        if (headItem.is(Items.PLAYER_HEAD)) {
+            CompoundTag tag = headItem.getTag();
+            if (tag != null && tag.contains(PlayerHeadItem.TAG_SKULL_OWNER)) {
+                GameProfile gameProfile = NbtUtils.readGameProfile(tag.getCompound(PlayerHeadItem.TAG_SKULL_OWNER));
+                if (!Objects.equals(player.getGameProfile(), gameProfile)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 
     static void renderPlayer(RenderPlayerEvent.Pre event) {
@@ -56,23 +94,67 @@ public class MineAnythingClientForge {
             if (tag != null && tag.contains(PlayerHeadItem.TAG_SKULL_OWNER)) {
                 GameProfile gameProfile = NbtUtils.readGameProfile(tag.getCompound(PlayerHeadItem.TAG_SKULL_OWNER));
                 if (!Objects.equals(player.getGameProfile(),gameProfile)) {
-                    PlayerInfo playerInfo = Minecraft.getInstance().getConnection().getPlayerInfo(gameProfile.getId());
-                    if (playerInfo != null) {
-                        PlayerRenderer playerRenderer = (PlayerRenderer) Minecraft.getInstance().getEntityRenderDispatcher().getSkinMap().get(playerInfo.getModelName());
 
-                        RemotePlayer fakePlayer = playerCache.computeIfAbsent(gameProfile.getId(),uuid -> new RemotePlayer((ClientLevel)player.level(), gameProfile));
+                    String modelInfo = getModelInfo(gameProfile);
 
-                        float yaw = Mth.lerp(event.getPartialTick(), player.yRotO, player.getYRot());
-;
-                        playerRenderer.render(fakePlayer,yaw,event.getPartialTick(),event.getPoseStack(),event.getMultiBufferSource(),event.getPackedLight());
+                    PlayerRenderer playerRenderer = (PlayerRenderer) Minecraft.getInstance().getEntityRenderDispatcher().getSkinMap().get(modelInfo);
 
+                    RemotePlayer fakePlayer = playerCache.computeIfAbsent(gameProfile.getId(),uuid -> new RemotePlayer((ClientLevel)player.level(), gameProfile));
 
-                        event.setCanceled(true);
+                    Player imitate = Minecraft.getInstance().level.getPlayerByUUID(gameProfile.getId());
+
+                    if (imitate != null) {
+                        for (EquipmentSlot slot : EquipmentSlot.values()) {
+                            fakePlayer.setItemSlot(slot,imitate.getItemBySlot(slot));
+                        }
                     }
+
+                    float yaw = Mth.lerp(event.getPartialTick(), player.yRotO, player.getYRot());
+
+                    fakePlayer.setXRot(player.getXRot());
+                    fakePlayer.setYRot(player.getYRot());
+
+                    fakePlayer.xRotO = player.xRotO;
+                    fakePlayer.yRotO = player.yRotO;
+
+                    fakePlayer.yHeadRotO = player.yHeadRotO;
+                    fakePlayer.yHeadRot = player.yHeadRot;
+
+                    fakePlayer.yBodyRotO = player.yBodyRotO;
+                    fakePlayer.yBodyRot = player.yBodyRot;
+                    fakePlayer.setDeltaMovement(player.getDeltaMovement());
+                    ;
+                    playerRenderer.render(fakePlayer,yaw,event.getPartialTick(),event.getPoseStack(),event.getMultiBufferSource(),event.getPackedLight());
+
+
+                    event.setCanceled(true);
                 }
             }
         }
     }
+
+    public static ResourceLocation getPlayerSkin(GameProfile gameProfile) {
+        Minecraft minecraft = Minecraft.getInstance();
+        SkinManager skinManager = minecraft.getSkinManager();
+        Map<MinecraftProfileTexture.Type, MinecraftProfileTexture> map = skinManager.getInsecureSkinInformation(gameProfile);
+        return map.containsKey(MinecraftProfileTexture.Type.SKIN) ? skinManager.registerTexture(map.get(MinecraftProfileTexture.Type.SKIN), MinecraftProfileTexture.Type.SKIN) :
+                DefaultPlayerSkin.getDefaultSkin(UUIDUtil.getOrCreatePlayerUUID(gameProfile));
+    }
+
+    public static String getModelInfo(GameProfile gameProfile) {
+
+        Minecraft minecraft = Minecraft.getInstance();
+        SkinManager skinManager = minecraft.getSkinManager();
+        Map<MinecraftProfileTexture.Type, MinecraftProfileTexture> map = skinManager.getInsecureSkinInformation(gameProfile);
+
+        if (!map.containsKey(MinecraftProfileTexture.Type.SKIN)) {
+            return DefaultPlayerSkin.getSkinModelName(gameProfile.getId());
+        } else {
+            String model = map.get(MinecraftProfileTexture.Type.SKIN).getMetadata("model");
+            return model != null ? model : "default";
+        }
+    }
+
 
     static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
         MineAnythingClient.registerRenderers();
