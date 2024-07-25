ec39c8a3002b4689b90e254facb3a7f2acac4114
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-07-25 03:17:16 -0400

    spawner patch

--- common/src/main/java/tfar/mineanything/mixin/SpawnEggItemMixin.java
+++ common/src/main/java/tfar/mineanything/mixin/SpawnEggItemMixin.java
@@ -1,8 +1,19 @@
 package tfar.mineanything.mixin;
 
 import net.minecraft.world.item.SpawnEggItem;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SpawnerBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(SpawnEggItem.class)
 public class SpawnEggItemMixin {
+
+    @Redirect(method = "useOn",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z"))
+    private boolean patchHardcode(BlockState instance, Block block) {
+        return instance.getBlock() instanceof SpawnerBlock;
+    }
 }
